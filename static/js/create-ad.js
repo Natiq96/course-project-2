250ac8a5c514212b0528ee5accad5a97e1b6975c
Natiq96 <thnth0906@gmail.com>
2025-09-09 02:57:45 +0400

    natigsh6

--- static/js/create-ad.js
+++ static/js/create-ad.js
@@ -1,50 +1,50 @@
-let citySection = document.querySelector(".citySection")
-let cityItem = document.querySelectorAll(".cityItem")
-let privateSection = document.querySelector(".privateCity")
-let input = document.querySelector(".searchInput")
 let menuMobile = document.querySelector(".menu-icon-mobile")
 let navMobileChild = document.querySelectorAll(".nav-child-mobile")
 let navChild = document.querySelectorAll(".nav-child")
 let activeMobile = document.querySelectorAll(".active-mobile")
 let navMobile = document.querySelector(".nav-mobile")
 let menuIconIsClick = document.querySelector(".menu-im-cl")
+let cityItemAd = document.querySelectorAll(".cityItem")
+let cityName = document.querySelector(".city-name")
 
 navChild.forEach(function(item) {
     item.addEventListener("click", function() {
-        navChild.forEach(el => {
-            el.classList.remove("active")
+        navChild.forEach(item => {
+            item.classList.remove("active")
         });
         item.classList.add("active");
     });
 });
+
 
 menuMobile.addEventListener("click", function () {
     navMobile.classList.toggle("nav-mobile-active")
     menuMobile.style.display = "none"
     menuIconIsClick.style.display = "flex"
 });
-
 menuIconIsClick.addEventListener("click", function () {
     navMobile.classList.toggle("nav-mobile-active")
     menuMobile.style.display = "flex"
     menuIconIsClick.style.display = "none"
 });
 
+// new
 
-citySection.addEventListener("click", function () {
-    if (privateSection.style.display === "none" || privateSection.style.display === "") {
-        privateSection.style.display = "block";
-    } else {
-        privateSection.style.display = "none";
+
+let chooseCity = document.getElementById("choose-public-element")
+let privateCity = document.getElementById("choose-priv-element")
+chooseCity.addEventListener("click", function(){    
+
+    if (privateCity.style.display === "" || privateCity.style.display === "none"){
+        privateCity.style.display = "flex"        
+    }else{
+        privateCity.style.display = "none"    
     }
-});
-
-
-cityItem.forEach(item => {
-    item.addEventListener("click", function () {
-        citySection.innerHTML = ""
-        citySection.innerHTML += `<p>${item.innerText}</p>`;
-        privateSection.style.display = "none"
-    })
 })
 
+cityItemAd.forEach(item => {
+    item.addEventListener("click", function() {
+        cityName.textContent = item.textContent; // city-name dəyişir
+        privateCity.style.display = "none"; // seçdikdən sonra dropdown bağlanır
+    });
+});
