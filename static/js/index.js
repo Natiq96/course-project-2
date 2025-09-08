fa3c28c6e4d32e24c3e6b5156acc05ac8519e069
Natiq96 <thnth0906@gmail.com>
2025-09-08 17:33:02 +0400

    natigsh3

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,27 @@
 let citySection = document.querySelector(".citySection")
 let cityItem = document.querySelectorAll(".cityItem")
 let privateSection = document.querySelector(".privateCity")
-let input  = document.querySelector(".searchInput")
+let input = document.querySelector(".searchInput")
+let menuMobile = document.querySelector(".menu-icon-mobile")
+let navMobileChild = document.querySelectorAll(".nav-child-mobile")
+let activeMobile = document.querySelectorAll(".active-mobile")
+let navMobile = document.querySelector(".nav-mobile")
+let menuIconIsClick = document.querySelector(".menu-im-cl")
+
+menuMobile.addEventListener("click", function () {
+    navMobile.classList.toggle("nav-mobile-active")
+    menuMobile.style.display = "none"
+    menuIconIsClick.style.display = "flex"
+});
+
+menuIconIsClick.addEventListener("click", function () {
+    navMobile.classList.toggle("nav-mobile-active")
+    menuMobile.style.display = "flex"
+    menuIconIsClick.style.display = "none"
+});
 
 
-
-
-citySection.addEventListener("click", function() {
+citySection.addEventListener("click", function () {
     if (privateSection.style.display === "none" || privateSection.style.display === "") {
         privateSection.style.display = "block";
     } else {
@@ -15,8 +30,8 @@ citySection.addEventListener("click", function() {
 });
 
 
-cityItem.forEach(item =>{
-    item.addEventListener("click",function(){
+cityItem.forEach(item => {
+    item.addEventListener("click", function () {
         citySection.innerHTML = ""
         citySection.innerHTML += `<p>${item.innerText}</p>`;
         privateSection.style.display = "none"
@@ -24,9 +39,9 @@ cityItem.forEach(item =>{
 })
 
 
-input.addEventListener("input", function() {
+input.addEventListener("input", function () {
     if (input.value.length >= 30) {
-        input.value = input.value.slice(0, 55); 
+        input.value = input.value.slice(0, 55);
         alert("Simvol sayı 55-dir!!!")
     }
 });
